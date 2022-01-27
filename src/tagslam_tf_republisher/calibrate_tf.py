e58f5d9fb1656e66cdbdbcd24ef2bc2345792f46
amyphung <amyngph@gmail.com>
2022-01-27 15:10:48 -0800

    Fix bugs - improve usability, all adjustments are in origin tag frame

--- src/tagslam_tf_republisher/calibrate_tf.py
+++ src/tagslam_tf_republisher/calibrate_tf.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 import rospy
 import tf2_ros
-from geometry_msgs.msg import Transform
+from geometry_msgs.msg import Transform, Vector3
 from std_msgs.msg import Bool
 
 # Because of transformations
 import tf_conversions
 from geometry_msgs.msg import Transform, TransformStamped, Quaternion
-from tf.transformations import quaternion_multiply
+import tf.transformations as tf_trans
 
 from tf_republisher.cfg import CalibrationConfig
 from dynamic_reconfigure.server import Server
 from utils import save_tf
+
 
 
 ## TODO: update package.xml with dependencies
@@ -40,16 +41,18 @@ From origin tag, make copy of tf, and publish tfs:
 
 BASE = "Base"
 TAGSLAM_BASE = "tagslam_base"
-TAGSLAM_CAMERA = "tagslam_kinect"
 ORIGIN_TAG = "tag_570"
-BASE_TAGS = ["tag_571", "tag_572", "tag_573"]
+TAGSLAM_CAMERA = "tagslam_kinect"
+TAGSLAM_ROOT = "map"
 CAMERA = "kinect2_rgb_optical_frame"
+
+BASE_TAGS = ["tag_571","tag_572","tag_573"]
 
 
 class TfCalibration():
 	def __init__(self):
 		rospy.init_node('calibrate_tf')
-		self.rate = rospy.Rate(10) # 10 Hz
+		self.rate = rospy.Rate(20) # 10 Hz
 		self.save_file = rospy.get_param("~save_file")
 
 		# Start dynamic reconfigure server
@@ -69,21 +72,9 @@ class TfCalibration():
 		self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
 		self.tf_broadcaster = tf2_ros.TransformBroadcaster()
 
-		# Save all times in one array: origin, base tags, camera
-		self._times = [0] * (len(BASE_TAGS) + 2)
-
-	def publish_tf(self, tf, idx):
-		# Check if we've already published this tf
-		tf_time = tf.header.stamp.to_sec()
-
-		if self._times[idx] != tf_time:
-			self._times[idx] = tf_time
-			self.tf_broadcaster.sendTransform(tf)
-
 	def run(self):
 		while not rospy.is_shutdown():
 			if self.done:
-				self.exit_pub.publish(Bool(True))
 				self.srv.update_configuration({"done": False})
 
 				# Save tf between base and kinect
@@ -93,47 +84,45 @@ class TfCalibration():
 				save_tf(t.transform, self.save_file)
 				break
 
+			# Continously update tagslam estimate for quick tf updates
+			for tag in BASE_TAGS:
+				try:
+					# Update tfs based on tagslam and republish with new header
+					tag_tf = self.tf_buffer.lookup_transform(TAGSLAM_BASE,
+															 tag,
+												   			 rospy.Time(0))
+					tag_tf.header.stamp = rospy.Time.now()
+					tag_tf.header.frame_id = BASE
+					tag_tf.child_frame_id = f"{tag}_copy"
+					self.tf_broadcaster.sendTransform(tag_tf)
+				except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+					rospy.logwarn_throttle(1, f"No tf between {TAGSLAM_BASE} and {tag}")
+					pass
+
 			try:
 				# Update tfs based on tagslam and republish with new header
 				origin_tf = self.tf_buffer.lookup_transform(TAGSLAM_BASE,
 															ORIGIN_TAG,
 											   				rospy.Time(0))
+				origin_tf.header.stamp = rospy.Time.now()
 				origin_tf.header.frame_id = BASE
 				origin_tf.child_frame_id = f"{ORIGIN_TAG}_copy"
-				self.publish_tf(origin_tf, 0)
+				self.tf_broadcaster.sendTransform(origin_tf)
 
 
 			except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
 				rospy.logwarn_throttle(1, f"No tf between {TAGSLAM_BASE} and {ORIGIN_TAG}")
 				pass
 
-			tag_tfs = [None] * len(BASE_TAGS)
-			for i, tag in enumerate(BASE_TAGS):
-				try:
-					tag_tfs[i] = self.tf_buffer.lookup_transform(TAGSLAM_BASE,
-																 tag,
-																 rospy.Time(0))
-					tag_tfs[i].header.frame_id = BASE
-					tag_tfs[i].child_frame_id = f"{tag}_copy"
-					self.publish_tf(tag_tfs[i], i+1)
-
-				except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
-					rospy.logwarn_throttle(1, f"No tf between {TAGSLAM_BASE} and {tag}")
-					pass
-
 			try:
-				camera_tf = self.tf_buffer.lookup_transform(ORIGIN_TAG,
-															TAGSLAM_CAMERA,
+				camera_tf = self.tf_buffer.lookup_transform(TAGSLAM_CAMERA,
+															ORIGIN_TAG,
 															rospy.Time(0))
 
 				# Update header info
+				camera_tf.header.stamp = rospy.Time.now()
 				camera_tf.header.frame_id = f"{ORIGIN_TAG}_copy"
 				camera_tf.child_frame_id = CAMERA
-
-				# Add tfs together
-				camera_tf.transform.translation.x += self.x
-				camera_tf.transform.translation.y += self.y
-				camera_tf.transform.translation.z += self.z
 
 				# Multiply quaternions
 				q1 = [camera_tf.transform.rotation.x,
@@ -145,9 +134,40 @@ class TfCalibration():
 																		  self.pitch,
 																		  self.yaw)
 
-				camera_tf.transform.rotation = Quaternion(*quaternion_multiply(q2, q1))
+				rot = tf_trans.quaternion_multiply(q2, q1)
 
-				self.publish_tf(camera_tf, -1)
+				camera_tf.transform.rotation = Quaternion(*tf_trans.quaternion_multiply(q1, q2))
+
+				trans = [camera_tf.transform.translation.x,
+					  	 camera_tf.transform.translation.y,
+					  	 camera_tf.transform.translation.z]
+
+				rot = [camera_tf.transform.rotation.x,
+					   camera_tf.transform.rotation.y,
+					   camera_tf.transform.rotation.z,
+					   camera_tf.transform.rotation.w]
+
+				# Invert tf
+				transform = tf_trans.concatenate_matrices(tf_trans.translation_matrix(trans),
+												 		  tf_trans.quaternion_matrix(rot))
+				inversed_transform = tf_trans.inverse_matrix(transform)
+
+				camera_tf.transform.translation = Vector3(*tf_trans.translation_from_matrix(inversed_transform))
+				camera_tf.transform.rotation = Quaternion(*tf_trans.quaternion_from_matrix(inversed_transform))
+
+				# Add tfs together
+				camera_tf.transform.translation.x += self.x
+				camera_tf.transform.translation.y += self.y
+				camera_tf.transform.translation.z += self.z
+
+				# Display tf
+				self.tf_broadcaster.sendTransform(camera_tf)
+
+				# # Update tagslam root
+				# camera_tf.header.frame_id = f"{ORIGIN_TAG}_copy"
+				# camera_tf.child_frame_id = TAGSLAM_ROOT
+				# self.tf_broadcaster.sendTransform(camera_tf)
+
 
 			except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
 				rospy.logwarn_throttle(1, f"No tf between {ORIGIN_TAG} and {TAGSLAM_CAMERA}")
